9462d7d337e5560b889764f2f63b9e5d25288e4d
Lincoln Dias <lincolnroberto820@gmail.com>
2024-09-18 12:59:14 -0300

    Tratamento de exceção para validação de campos

---
+++ src/main/java/com/parkapi/park/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.parkapi.park.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorMessage> MethodArgumentNotValidException(MethodArgumentNotValidException ex,
+			                                                            HttpServletRequest request,
+			                                                            BindingResult result){
+		return ResponseEntity
+				.status(HttpStatus.UNPROCESSABLE_ENTITY)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) envalidos", result));
+	}
+}
